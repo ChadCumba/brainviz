1abca6d9c20e7788896a16cfb5c0b34f52e25db6
root <root@euphoria.(none)>
2011-03-01 13:54:50 -0600

    added settings/urls for live

--- brainviz/settings.py
+++ brainviz/settings.py
@@ -7,19 +7,19 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@domain.com'),
+     ('Chad Cumba', 'chad.cumba@mail.utexas.edu'),
 )
 
 MANAGERS = ADMINS
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/Users/chadcumba/Documents/Aptana Studio 3 Workspace/brainviz/sqlite.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'django_brainviz',  # Or path to database file if using sqlite3.
+        'USER': 'django_brainviz',  # Not used with sqlite3.
+        'PASSWORD': 'br41n80x',     # Not used with sqlite3.
+        'HOST': '',                 # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                 # Set to empty string for default. Not used with sqlite3.
     }
 }
 
@@ -54,12 +54,12 @@ MEDIA_ROOT = os.path.join(CURRENT_DIR, 'static')
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash if there is a path component (optional in other cases).
 # Examples: "http://media.lawrence.com", "http://example.com/media/"
-MEDIA_URL = '/static/'
+MEDIA_URL = '/brainviz/static/'
 
 # URL prefix for admin media -- CSS, JavaScript and images. Make sure to use a
 # trailing slash.
 # Examples: "http://foo.com/media/", "/media/".
-ADMIN_MEDIA_PREFIX = '/media/'
+ADMIN_MEDIA_PREFIX = 'http://euphoria.clm.utexas.edu/brainviz/media/'
 
 # Make this unique, and don't share it with anybody.
 SECRET_KEY = '0rgrrt*nsr$gr(+(r&3_ld8%$bu8$zojmtqy=_p12czz3j##p&'
