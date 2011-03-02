fd9415c80a11e485e6479d9c2cea341324efe180
Chad Cumba <chad.cumba@mail.utexas.edu>
2011-03-02 14:52:38 -0600

    Removed while loop from getQueryParams as safari and chrome had difficulty interpreting the regular expression. Both of those browsers simply returned true for the expression and thus looped forever.

--- brainviz/static/javascripts/urlParameters.js
+++ brainviz/static/javascripts/urlParameters.js
@@ -6,10 +6,15 @@ function getQueryParams(qs) {
     var params = {};
     var tokens;
 
-    while (tokens = /[?&]?([^=]+)=([^&]*)/g.exec(qs)) {
-        params[decodeURIComponent(tokens[1])]
-            = decodeURIComponent(tokens[2]);
-    }
-
-    return params;
+	qs = qs.slice(1);
+	queryset = qs.split('&');
+	
+	for(var i = 0; i < queryset.length; i++){
+		tokens = queryset[i].split('=');
+		params[decodeURIComponent(tokens[0])] = 
+			decodeURIComponent(tokens[1]);
+	}
+	
+	return params;
+	
 }
